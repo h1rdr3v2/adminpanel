5e9a3680aaceb98797011d62459aabea686231f8
HafrikPlay <hafrikplay@gmail.com>
2024-04-05 09:39:57 +0100

    Update .prettierrc and Logout.jsx

--- src/routes/Logout.jsx
+++ src/routes/Logout.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import { useAuth } from '../contexts/AuthContext';
-import { useBot } from '../contexts/BotContext';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { useBot } from "../contexts/BotContext";
 
 const Logout = () => {
-  const navigate = useNavigate();
   const { logout } = useAuth();
   const { clearBot } = useBot();
   logout();
   clearBot();
-  window.location.href = '/';
+  window.location.href = "/";
   return <></>;
 };
 
